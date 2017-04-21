d65cbf764a7b0705a6b834ae024b000b003eddea
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2017-04-21 09:29:20 -0700

    began test setup

--- lib-functional/index.js
+++ lib-functional/index.js
@@ -11,36 +11,36 @@ module.exports = function parseFunctional(fileArray) {
   })
   return result.join('\n')
 }
-function addH1(input){
+export default function addH1(input){
   return input.replace(/^\s*(?:\<h1\>)*(?:\#\s)(?!\#)(.*)/gm, '<h1>$1</h1>')
 }
-function addH2(input){
+export default function addH2(input){
   return input.replace(/^\s*(?:\<h2\>)*(?:\##\s)(?!\#)(.*)/gm, '<h2>$1</h2>')
 }
-function addH3(input){
+export default function addH3(input){
   return input.replace(/^\s*(?:\<h3\>)*(?:\###\s)(?!\#)(.*)/gm, '<h3>$1</h3>')
 }
-function addH4(input){
+export default function addH4(input){
   return input.replace(/^\s*(?:\<h4\>)*(?:\####\s)(?!\#)(.*)/gm, '<h4>$1</h4>')
 }
-function addH5(input){
+export default function addH5(input){
   return input.replace(/^\s*(?:\<h5\>)*(?:\#####\s)(?!\#)(.*)/gm, '<h5>$1</h5>')
 }
-function addH6(input){
+export default function addH6(input){
   return input.replace(/^\s*(?:\<h6\>)*(?:\######\s)(?!\#)(.*)/gm, '<h6>$1</h6>')
 }
-function addBold(input){
+export default function addBold(input){
   return input.replace(/(?:(?:\*\*)|(?:\_\_))*(.+?)(?:(\*\*)|(?:\_\_))/gm, '<strong>$1</strong>')
 }
-function addItalics(input){
+export default function addItalics(input){
   return input.replace(/(?:(?:\*)|(?:\_))*(.+?)(?:(\*)|(?:\_))/gm, '<i>$1</i>')
 }
-function addLinks(input){
+export default function addLinks(input){
   return input.replace(/(^(?:(\[(.+?)\]))*(?:\((.*?)\)))/gm, '<a href=\"$4\">$3</a>')
 }
-function addImages(input){
+export default function addImages(input){
   return input.replace(/(?:!(?:(\[(.+?)\]))*(?:\((.*?)\)))/gm, '<img src="$3" alt="$2">')
 }
-function addListItem(input){
+export default function addListItem(input){
   return input.replace(/(?:(\- )(.*))/gm, '<li>$2</li>')
 }
---
+++ spec/functional_test.js
@@ -0,0 +1,1 @@
+import { addH1, addH2, addH3, addH4, addH5, addH6, addBold, addItalics, addLinks, addImages, addListItem } from '../lib-functional/index'
